963bb41474bcd1234230bb49db2cd5b4b23fdaec
Ezequiel Sousa <63311919+Escowolf@users.noreply.github.com>
2022-04-26 00:05:51 -0300

    Update Login.js

--- src/Paginas/Login.js
+++ src/Paginas/Login.js
@@ -1,24 +1,107 @@
-import axios from "axios";
+import { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from "./context/AuthProvider";
 
-function getPlaylist(id) {
-    axios.get(`https://localhost:4000/playlists/${id}`)
-      .then(response => response.json())
-      .then(data => console.log(data.nome))
-      .catch(error => console.log(error))
-      .finally(() => { "aviso de fim de carregamento" })
-   }
-   
-   getUser(1);
+import axios from 'axios';
+const LOGIN_URL = '/auth';
 
-axios.get("http://localhost:3000/playlists")
-     .then(response => setPlaylists(response.data));
-     
-function Login() {
+const Login = () => {
+    const { setAuth } = useContext(AuthContext);
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post(LOGIN_URL,
+                JSON.stringify({ user, pwd }),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+            console.log(JSON.stringify(response?.data));
+            //console.log(JSON.stringify(response));
+            const accessToken = response?.data?.accessToken;
+            const roles = response?.data?.roles;
+            setAuth({ user, pwd, roles, accessToken });
+            setUser('');
+            setPwd('');
+            setSuccess(true);
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('Sem resposta do servidor');
+            } else if (err.response?.status === 400) {
+                setErrMsg('Usuário ou senha errada');
+            } else if (err.response?.status === 401) {
+                setErrMsg('Não autorizado');
+            } else {
+                setErrMsg('Falha no login');
+            }
+            errRef.current.focus();
+        }
+    }
+
     return (
-     <>
-     
-     </>
-    );
+        <>
+            {success ? (
+                <section>
+                    <h1>You are logged in!</h1>
+                    <br />
+                    <p>
+                        <a href="#">Go to Home</a>
+                    </p>
+                </section>
+            ) : (
+                <section>
+                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                    <h1>Sign In</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="username">Username:</label>
+                        <input
+                            type="text"
+                            id="username"
+                            ref={userRef}
+                            autoComplete="off"
+                            onChange={(e) => setUser(e.target.value)}
+                            value={user}
+                            required
+                        />
+
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            onChange={(e) => setPwd(e.target.value)}
+                            value={pwd}
+                            required
+                        />
+                        <button>Sign In</button>
+                    </form>
+                    <p>
+                        Need an Account?<br />
+                        <span className="line">
+                            {/*put router link here*/}
+                            <a href="#">Sign Up</a>
+                        </span>
+                    </p>
+                </section>
+            )}
+        </>
+    )
 }
 
 export default Login;
